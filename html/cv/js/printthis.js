41890c8e3adf20fac6fdb280bf5c2c796cacde9e
V S Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2021-10-13 09:30:56 +0530

    New Update

--- html/cv/js/printthis.js
+++ html/cv/js/printthis.js
@@ -1,38 +1,38 @@
 function scaleCv() {
-          document.body.classList.add('scale-cv');
+  document.body.classList.add("scale-cv");
 }
 
 function removeScale() {
-          document.body.classList.remove('scale-cv');
+  document.body.classList.remove("scale-cv");
 }
 
-let resumeButton = document.getElementById('resumeButton');
+let resumeButton = document.getElementById("resumeButton");
 
-var pdf = document.getElementById('resume');
+var pdf = document.getElementById("resume");
 var opt = {
-          margin: 5,
-          filename: 'sonusainiCV.pdf',
-          image: { type: 'jpeg', quality: 0.98 },
-          html2canvas: { scale: 5 },
-          jsPDF: { format: 'a4', orientation: 'portrait' }
+  margin: 5,
+  filename: "sonusainiCV.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 5 },
+  jsPDF: { format: "a4", orientation: "portrait" },
 };
 
-resumeButton.addEventListener('click', function () {
-          scaleCv();
+resumeButton.addEventListener("click", function () {
+  scaleCv();
 
-          print();
+  print();
 
-          setTimeout(removeScale, 1000);
+  setTimeout(removeScale, 1000);
 });
 function print() {
-          html2pdf(pdf, opt);
+  html2pdf(pdf, opt);
 }
 
 var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var yyyy = today.getFullYear();
 
-today = dd + '/' + mm + '/' + yyyy;
+today = dd + "/" + mm + "/" + yyyy;
 
-document.getElementById('date').innerHTML = today;
+document.getElementById("date").innerHTML = today;
