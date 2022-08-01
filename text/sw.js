8783ed4e87970ceb5bed518e53fe65a6aa8429e4
Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2022-08-01 21:55:58 +0530

    Add Text

--- text/sw.js
+++ text/sw.js
@@ -1,48 +1,23 @@
-const CACHE_NAME = "V5";
-const STATIC_CACHE_URLS = ["/", "./index.html", "/app.js"];
+const staticCacheName = "site-static";
+const assets = ["/", "/index.html", "/js/app.js", "/js/ui.js", "/js/materialize.min.js", "/css/styles.css", "/css/materialize.min.css", "/img/dish.png", "https://fonts.googleapis.com/icon?family=Material+Icons", "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"];
 
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing.");
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_CACHE_URLS)));
-});
-
-self.addEventListener("fetch", (event) => {
-  // Stratégie Cache-First
-  event.respondWith(
-    caches
-      .match(event.request) // On vérifie si la requête a déjà été mise en cache
-      .then((cached) => cached || fetch(event.request)) // sinon on requête le réseau
-      .then(
-        (response) =>
-          cache(event.request, response) // on met à jour le cache
-            .then(() => response) // et on résout la promesse avec l'objet Response
-      )
+// install event
+self.addEventListener("install", (evt) => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  // delete any unexpected caches
-  event.waitUntil(
-    caches
-      .keys()
-      .then((keys) => keys.filter((key) => key !== CACHE_NAME))
-      .then((keys) =>
-        Promise.all(
-          keys.map((key) => {
-            console.log(`Deleting cache ${key}, New Cache ${CACHE_NAME} `);
-            return caches.delete(key);
-          })
-        )
-      )
-  );
+// activate event
+self.addEventListener("activate", (evt) => {
+  //console.log('service worker activated');
 });
 
-function cache(request, response) {
-  if (response.type === "error" || response.type === "opaque") {
-    return Promise.resolve(); // do not put in cache network errors
-  }
-
-  return caches.open(CACHE_NAME).then((cache) => cache.put(request, response.clone()));
-}
-
-// installs;
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  //console.log('fetch event', evt);
+});
