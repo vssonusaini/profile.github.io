f31a9680cdaf173707f9b2c5b6240c0a57c2f48c
Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2022-08-01 18:38:38 +0530

    Add root

--- html/online-qrcode-generator-tool/sw.js
+++ html/online-qrcode-generator-tool/sw.js
@@ -1,11 +1,22 @@
 console.log("Index Service Worker Run");
 
 let cacheVer = "qrcode_generator_0.1v";
+let root = "https://sainisahab.com/html/fake-twitter-image-generator";
+cache_file = [
+  `${root}https://sainisahab.com/html/fake-twitter-image-generator/index.html`, 
+  `${root}/js/qrcode.min.js`,
+  `${root}/js/html2canvas.min.js`, 
+  `${root}/js/barcode.js`, 
+  `${root}/js/main.js`, 
+  `${root}/css/style.css`, 
+  `${root}/css/root.css`, 
+  `${root}/sw.js`
+];
 
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheVer).then((cache) => {
-      cache.addAll(["index.html", "/js/qrcode.min.js", "https://sainisahab.com/html/fake-twitter-image-generator/js/html2canvas.min.js", "/js/barcode.js", "/js/main.js", "/css/style.css", "/css/root.css", "/sw.js"]);
+      cache.addAll(cache_file);
     })
   );
 });
