bae47a02b82f217b4998050dee7c402ed91325b8
Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2022-08-02 11:22:44 +0530

    Update sw.js

--- html/online-qrcode-generator-tool/sw.js
+++ html/online-qrcode-generator-tool/sw.js
@@ -1,11 +1,11 @@
-const staticCacheName = "site-static";
+const staticCacheName = "v0.2";
 const assets = ["/"];
 
 cache_file = ["/index.html"];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open("v1").then(function (cache) {
+    caches.open(staticCacheName).then(function (cache) {
       return cache.addAll(cache_file);
     })
   );
@@ -26,7 +26,7 @@ self.addEventListener("fetch", function (event) {
             // and serve second one
             let responseClone = response.clone();
 
-            caches.open("v0.1").then(function (cache) {
+            caches.open(staticCacheName).then(function (cache) {
               cache.put(event.request, responseClone);
             });
             return response;
@@ -38,24 +38,3 @@ self.addEventListener("fetch", function (event) {
     })
   );
 });
-
-// // install event
-// self.addEventListener("install", (evt) => {
-//     //console.log('service worker installed');
-//     evt.waitUntil(
-//       caches.open(staticCacheName).then((cache) => {
-//         console.log("caching shell assets");
-//         cache.addAll(assets);
-//       })
-//     );
-//   });
-
-//   // activate event
-//   self.addEventListener("activate", (evt) => {
-//     //console.log('service worker activated');
-//   });
-
-//   // fetch event
-//   self.addEventListener("fetch", (evt) => {
-//     //console.log('fetch event', evt);
-//   });
