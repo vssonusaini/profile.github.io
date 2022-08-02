4d749a1b244fbfa0208716e5071edcb09c3d1791
Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2022-08-02 11:50:05 +0530

    Update sw.js

--- html/online-qrcode-generator-tool/sw.js
+++ html/online-qrcode-generator-tool/sw.js
@@ -1,56 +1,23 @@
-const CACHE_NAME = "v0.4";
-cache_file = ["/index.html"];
+const CACHE_NAME = "V5";
+const STATIC_CACHE_URLS = ["/"];
 
-self.addEventListener("install", function (evnt) {
-  evnt.waitUntil(
+// install event
+self.addEventListener("install", (evt) => {
+  //console.log('service worker installed');
+  evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("caching shell assets");
-      cache.addAll(cache_file);
+      cache.addAll(STATIC_CACHE_URLS);
     })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  // delete any unexpected caches
-  event.waitUntil(
-    caches
-      .keys()
-      .then((keys) => keys.filter((key) => key !== CACHE_NAME))
-      .then((keys) =>
-        Promise.all(
-          keys.map((key) => {
-            console.log(`Deleting cache ${key}, New Cache ${CACHE_NAME} `);
-            return caches.delete(key);
-          })
-        )
-      )
-  );
+// activate event
+self.addEventListener("activate", (evt) => {
+  //console.log('service worker activated');
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request)
-          .then(function (response) {
-            // response may be used only once
-            // we need to save clone to put one copy in cache
-            // and serve second one
-            let responseClone = response.clone();
-
-            caches.open(CACHE_NAME).then(function (cache) {
-              cache.put(event.request, responseClone);
-            });
-            return response;
-          })
-          .catch(function () {
-            return caches.match("/html/online-qrcode-generator-tool.jpg");
-          });
-      }
-    })
-  );
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  //console.log('fetch event', evt);
 });
