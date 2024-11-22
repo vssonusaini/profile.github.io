f96830b892da5e142068dcd5cb98ca3b2ccfa540
Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2024-11-23 01:50:10 +0530

    Update custom-scripts.js

--- html/online-games/memory-tast-game/custom-scripts.js
+++ html/online-games/memory-tast-game/custom-scripts.js
@@ -15,49 +15,53 @@
 //     }
 // });
 
-
 const showStickyAd = () => {
-    const stickyBtmAd = document.querySelector(".sticky-bottom-ad");
-    const closeBtn = stickyBtmAd.querySelector(".close-btn");
-    const googleAd = stickyBtmAd.querySelector(".adsbygoogle");
-    const isAdLoaded = googleAd ?.getAttribute("data-ad-status");
-    const isStickyHidden = stickyBtmAd.classList.contains("hide");
-    if (window.innerWidth < 700 || isStickyHidden || !isAdLoaded || isAdLoaded === "unfilled") return;
+  const stickyBtmAd = document.querySelector(".sticky-bottom-ad");
+  const closeBtn = stickyBtmAd.querySelector(".close-btn");
+  const googleAd = stickyBtmAd.querySelector(".adsbygoogle");
+  const isAdLoaded = googleAd?.getAttribute("data-ad-status");
+  const isStickyHidden = stickyBtmAd.classList.contains("hide");
+  if (
+    window.innerWidth < 700 ||
+    isStickyHidden ||
+    !isAdLoaded ||
+    isAdLoaded === "unfilled"
+  )
+    return;
 
-    stickyBtmAd.classList.add("show");
+  stickyBtmAd.classList.add("show");
 
-    const hideStickyAd = () => {
-        stickyBtmAd.classList.remove("show");
-    }
+  const hideStickyAd = () => {
+    stickyBtmAd.classList.remove("show");
+  };
 
-    closeBtn.addEventListener("click", hideStickyAd);
-}
+  closeBtn.addEventListener("click", hideStickyAd);
+};
 
 const insertStickyAd = () => {
-    const stickyBtmAd = document.createElement("div");
-    stickyBtmAd.classList.add("sticky-bottom-ad");
-    stickyBtmAd.innerHTML = `<div class="ad-body">
-                                <div class="close-btn">
-                                    <svg viewBox="0 0 48 48" fill="#333"><path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z"></path><path d="M0 0h48v48H0z" fill="none"></path></svg>
-                                </div>
-                              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3966268377883628"
-                                    crossorigin="anonymous"></script>
-                                <!-- memory-tast-game -->
-                                <ins class="adsbygoogle"
-                                    style="display:inline-block;width:728px;height:90px"
-                                    data-ad-client="ca-pub-3966268377883628"
-                                    data-ad-slot="9323209910"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>`;
+  //   const stickyBtmAd = document.createElement("div");
+  //   stickyBtmAd.classList.add("sticky-bottom-ad");
+  //   stickyBtmAd.innerHTML = `<div class="ad-body">
+  //                                 <div class="close-btn">
+  //                                     <svg viewBox="0 0 48 48" fill="#333"><path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z"></path><path d="M0 0h48v48H0z" fill="none"></path></svg>
+  //                                 </div>
+  //                               <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3966268377883628"
+  //                                     crossorigin="anonymous"></script>
+  //                                 <!-- memory-tast-game -->
+  //                                 <ins class="adsbygoogle"
+  //                                     style="display:inline-block;width:728px;height:90px"
+  //                                     data-ad-client="ca-pub-3966268377883628"
+  //                                     data-ad-slot="9323209910"></ins>
+  //                                 <script>
+  //                                     (adsbygoogle = window.adsbygoogle || []).push({});
+  //                                 </script>
+  //                             </div>`;
 
-    document.body.appendChild(stickyBtmAd);
-    
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    setTimeout(() => showStickyAd(), 3000);
-}
+  //   document.body.appendChild(stickyBtmAd);
 
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  setTimeout(() => showStickyAd(), 3000);
+};
 
 insertStickyAd();
 // let isHideVignette = false;
@@ -147,7 +151,6 @@ insertStickyAd();
 //             gtag('js', new Date());
 //             gtag('config', 'G-YGFQCTFP4H');`;
 //     document.head.appendChild(gtagConfigScript);
-
 
 //     adScript.onload = () => {
 //         // insertVignetteAd();
