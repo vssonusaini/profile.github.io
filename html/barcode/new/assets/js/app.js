49cde38f0b0199388e7f4dc2a59d09af56f86718
Sonu Saini <63990140+vssonusaini@users.noreply.github.com>
2024-12-05 01:16:55 +0530

    update

--- html/barcode/new/assets/js/app.js
+++ html/barcode/new/assets/js/app.js
@@ -1,5 +1,5 @@
 // btn
-const CartPositionBTN = document.getElementById("CTPO");
+// const CartPositionBTN = document.getElementById("CTPO");
 const BarcodeBTN = document.getElementById("BR");
 const QRCodeBTN = document.getElementById("QR");
 
@@ -17,12 +17,12 @@ if (DB === null) {
  localStorage.setItem("Database", JSON.stringify(DB));
 }
 
-CartPositionBTN.addEventListener("click", () => {
- let host_setting = JSON.parse(localStorage.getItem("Database"));
- host_setting.ActiveTab = "CTPO";
- localStorage.setItem("Database", JSON.stringify(host_setting));
- CheckActive();
-});
+// CartPositionBTN.addEventListener("click", () => {
+//  let host_setting = JSON.parse(localStorage.getItem("Database"));
+//  host_setting.ActiveTab = "CTPO";
+//  localStorage.setItem("Database", JSON.stringify(host_setting));
+//  CheckActive();
+// });
 
 BarcodeBTN.addEventListener("click", () => {
  let host_setting = JSON.parse(localStorage.getItem("Database"));
@@ -42,7 +42,7 @@ const CheckActive = () => {
  let host_setting = JSON.parse(localStorage.getItem("Database"));
 
  if (host_setting.ActiveTab == "CTPO") {
-  CartPositionBTN.className = "active";
+//   CartPositionBTN.className = "active";
   BarcodeBTN.classList.remove("active");
   QRCodeBTN.classList.remove("active");
   CartPositionblock.style.display = "block";
